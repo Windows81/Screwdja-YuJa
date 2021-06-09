c97c4ef38ca947c5470ec98e8791b7d596b6d3dd
Windows81 <nastyahmede@gmail.com>
2021-06-09 19:35:01 +0000

    Sixth commit.

--- .ps1
+++ .ps1
@@ -1,5 +1,5 @@
 Param(
-	[bool]$play = 0, [int]$incr = 1, [int]$ss = $incr + ((gc yuja.csv -tail 2 -Delimiter "`r")[0] -replace "^\s*(\d+)[^\0]+", "`$1"), [int]$stop = $ss - $incr)
+	[bool]$play = 0, [Parameter(Mandatory = 1)][int]$incr = -1, [Parameter(Mandatory = 1)][int]$ss, [int]$stop = $ss - $incr)
 
 for ($n = $ss; $n -ne $stop; $n += $incr) {
 	$c = (iwr "https://uci.yuja.com/V/Video?v=$n").content;
@@ -8,9 +8,11 @@ for ($n = $ss; $n -ne $stop; $n += $incr) {
 	if ($p) {
 		$v = [regex]::Match((iwr $p -Method Head -MaximumRedirection 0 -SkipHttpErrorCheck -ErrorAction SilentlyContinue ).headers.location, 'Video-\w{8}-\w{4}-\w{4}-\w{4}-\w{12}').value;
 		if ($v) {
+			$ns = [string]$n;
+			$d = $ns[0];
 			$l = "https://coursecast-chatter.s3.us-west-2.amazonaws.com/$($v)_processed.mp4";
 			echo $n $l $t "";
-			ac yuja.csv "$n,`"$t`",`"$l`",";
+			ac "yuja$d.csv" "$($ns.PadLeft(7,'0')),`"$t`",`"$l`",";
 			if ($play) { start "C:\Program Files\VideoLAN\VLC\vlc.exe" $l -Wait }
 		}
 	}
--- .py
+++ .py
@@ -4,70 +4,76 @@ from matplotlib import rcParams
 import regex
 import csv
 import sys
+import glob
 
-with open("yuja.csv", "r", encoding="cp850") as f:
-    s = csv.DictReader(f, delimiter=",", quotechar='"')
-    rcParams["toolbar"] = "None"
-    dates, dates_flt, dates_ids = [], [], []
-    o = input("Enter a program ID: ")
+f = [open(n, "r", encoding="cp850") for n in glob.glob("yuja?.csv")]
+g = (str(l) for i, s in enumerate(f) for j, l in enumerate(s) if j > 0 or i == 0)
+s = csv.DictReader(g, delimiter=",", quotechar='"')
 
-    if o == "dateplot":
-        for r in s:
-            try:
-                m = regex.search("GMT(\\d{8}-\\d{6})", r["name"])
-                if m:
-                    d = datetime.strptime(m.groups()[0], "%Y%m%d-%H%M%S")
-                    dates.append(tuple((int(r["id"]), d)))
-            except Exception:
-                pass
+dates, dates_flt, dates_ids = [], [], []
+o = input("Enter a program ID: ")
+rcParams["toolbar"] = "None"
 
-        dates.sort()
-        for [i, d] in dates:
-            if not len(dates_flt) or dates_flt[-1] < d:
-                dates_ids.append(i)
-                dates_flt.append(d)
+if o == "dateplot":
+    for r in s:
+        try:
+            m = regex.search("GMT(\\d{8}-\\d{6})", r["name"])
+            if m:
+                d = datetime.strptime(m.groups()[0], "%Y%m%d-%H%M%S")
+                dates.append(tuple((int(r["id"]), d)))
+        except Exception:
+            pass
 
-        p = pyplot.plot_date(dates_flt, dates_ids, "-")
-        pyplot.ticklabel_format(axis="y", style="plain")
-        pyplot.xticks(rotation=90)
-        p[0].figure.tight_layout()
-        pyplot.show()
+    dates.sort()
+    for [i, d] in dates:
+        if not len(dates_flt) or dates_flt[-1] < d:
+            dates_ids.append(i)
+            dates_flt.append(d)
 
-    elif o == "diffplot":
-        ids = sorted([int(r["id"]) for r in s])
-        p = pyplot.plot(ids[1:], [b - a for a, b in zip(ids, ids[1:])])
-        pyplot.ticklabel_format(style="plain")
-        p[0].figure.tight_layout()
-        pyplot.show()
+    p = pyplot.plot_date(dates_flt, dates_ids, "-")
+    pyplot.ticklabel_format(axis="y", style="plain")
+    pyplot.xticks(rotation=90)
+    p[0].figure.tight_layout()
+    pyplot.show()
 
-    elif o == "hist":
-        ids = sorted([int(r["id"]) for r in s])
-        p = pyplot.hist(ids, 200)
-        pyplot.ticklabel_format(style="plain")
-        pyplot.show()
+elif o == "diffplot":
+    ids = sorted([int(r["id"]) for r in s])
+    p = pyplot.plot(ids[1:], [b - a for a, b in zip(ids, ids[1:])])
+    pyplot.ticklabel_format(style="plain")
+    p[0].figure.tight_layout()
+    pyplot.show()
 
-    elif o == "dateprop":
-        ids = sorted([int(r["id"]) for r in s])
-        props = [i / (v - ids[0]) for i, v in enumerate(ids[10:], 10)]
-        p = pyplot.plot(ids[10:], props)
-        pyplot.ticklabel_format(axis="x", style="plain")
-        p[0].figure.tight_layout()
-        pyplot.show()
+elif o == "hist":
+    ids = sorted([int(r["id"]) for r in s])
+    p = pyplot.hist(ids, 200)
+    pyplot.ticklabel_format(style="plain")
+    pyplot.show()
 
-    elif o == "dateprop-rev":
-        ids = sorted([int(r["id"]) for r in s], reverse=True)
-        props = [i / (ids[0] - v) for i, v in enumerate(ids[10:], 10)]
-        p = pyplot.plot(ids[10:], props)
-        pyplot.ticklabel_format(axis="x", style="plain")
-        p[0].figure.tight_layout()
-        pyplot.show()
+elif o == "dateprop":
+    ids = sorted([int(r["id"]) for r in s])
+    props = [i / (v - ids[0]) for i, v in enumerate(ids[10:], 10)]
+    p = pyplot.plot(ids[10:], props)
+    pyplot.ticklabel_format(axis="x", style="plain")
+    p[0].figure.tight_layout()
+    pyplot.show()
 
-    elif o == "bounds":
-        mn, mx = sys.maxsize, 0
-        for r in s:
-            i = int(r["id"])
-            if mn > i:
-                mn = i
-            if mx < i:
-                mx = i
-        print(mn, mx)
+elif o == "dateprop-rev":
+    ids = sorted([int(r["id"]) for r in s], reverse=True)
+    props = [i / (ids[0] - v) for i, v in enumerate(ids[10:], 10)]
+    p = pyplot.plot(ids[10:], props)
+    pyplot.ticklabel_format(axis="x", style="plain")
+    p[0].figure.tight_layout()
+    pyplot.show()
+
+elif o == "bounds":
+    mn, mx = sys.maxsize, 0
+    for r in s:
+        i = int(r["id"])
+        if mn > i:
+            mn = i
+        if mx < i:
+            mx = i
+    print(mn, mx)
+
+for s in f:
+    del s
