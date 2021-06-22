a4fa68222ba11ce83a71184ce3440e0e04661cee
Windows81 <nastyahmede@gmail.com>
2021-06-22 08:12:39 +0000

    Eighth commit.

--- .py
+++ .py
@@ -1,80 +1,55 @@
-from datetime import datetime
-from matplotlib import pyplot
-from matplotlib import rcParams
-import regex
-import csv
-import sys
-import glob
+import threading
+import requests
+import argparse
+import html
+import re
 
-f = [open(n, "r", encoding="cp850") for n in glob.glob("yuja?.csv")]
-g = (str(l) for i, s in enumerate(f) for j, l in enumerate(s) if j > 0 or i == 0)
-s = csv.DictReader(g, delimiter=",", quotechar='"')
 
-dates, dates_flt, dates_ids = [], [], []
-o = sys.argv[1] if len(sys.argv) > 1 else input("Enter a program ID: ")
-rcParams["toolbar"] = "None"
+def process(g):
+    for n in g:
+        c = requests.get(
+            f"https://uci.yuja.com/V/Video?v={n}",
+        ).text
+        p = re.search(r"https://uci.yuja.com/P/DataPage/BroadcastsThumb/\d+", c)
+        if not p:
+            continue
 
-if o == "dateplot":
-    for r in s:
-        try:
-            m = regex.search("GMT(\\d{8}-\\d{6})", r["name"])
-            if m:
-                d = datetime.strptime(m.groups()[0], "%Y%m%d-%H%M%S")
-                dates.append(tuple((int(r["id"]), d)))
-        except Exception:
-            pass
+        m = re.search(
+            r'<meta property="og:title" content="(.+)" />',
+            c,
+        )
+        t = html.unescape(m.group(1))
 
-    dates.sort()
-    for [i, d] in dates:
-        if not len(dates_flt) or dates_flt[-1] < d:
-            dates_ids.append(i)
-            dates_flt.append(d)
+        v = re.search(
+            r"Video-\w{8}-\w{4}-\w{4}-\w{4}-\w{12}",
+            requests.head(p.group(0)).headers.get("location", ""),
+        )
+        if not v:
+            continue
 
-    p = pyplot.plot_date(dates_flt, dates_ids, "-")
-    pyplot.ticklabel_format(axis="y", style="plain")
-    pyplot.xticks(rotation=90)
-    p[0].figure.tight_layout()
-    pyplot.show()
+        ns = str(n).rjust(7, "0")
+        l = f"https://coursecast-chatter.s3.us-west-2.amazonaws.com/{v.group(0)}_processed.mp4"
+        yield (ns, l, t)
 
-elif o == "diffplot":
-    ids = sorted([int(r["id"]) for r in s])
-    p = pyplot.plot(ids[1:], [b - a for a, b in zip(ids, ids[1:])])
-    pyplot.ticklabel_format(style="plain")
-    p[0].figure.tight_layout()
-    pyplot.show()
 
-elif o == "hist":
-    ids = sorted([int(r["id"]) for r in s])
-    p = pyplot.hist(ids, 200)
-    pyplot.ticklabel_format(style="plain")
-    pyplot.show()
+def write(g):
+    for (ns, l, t) in process(g):
+        with open(f"yuja{ns[0]}.csv", "a") as f:
+            f.write(f'{ns},"{t}","{l}",\n')
+            print(ns, l, t, "")
 
-elif o == "dateprop":
-    ids = sorted([int(r["id"]) for r in s])
-    props = [i / (v - ids[0]) for i, v in enumerate(ids[10:], 10)]
-    p = pyplot.plot(ids[10:], props)
-    pyplot.ticklabel_format(axis="x", style="plain")
-    p[0].figure.tight_layout()
-    pyplot.show()
 
-elif o == "dateprop-rev":
-    ids = sorted([int(r["id"]) for r in s], reverse=True)
-    props = [i / (ids[0] - v) for i, v in enumerate(ids[10:], 10)]
-    p = pyplot.plot(ids[10:], props)
-    pyplot.ticklabel_format(axis="x", style="plain")
-    p[0].figure.tight_layout()
-    pyplot.show()
-
-elif o == "bounds":
-    mn, mx = sys.maxsize, 0
-    for r in s:
-        i = int(r["id"])
-        if mn > i:
-            mn = i
-        if mx < i:
-            mx = i
-    print(mn)
-    print(mx)
-
-for s in f:
-    del s
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-incr", default=-1, required=False, type=int)
+    parser.add_argument("-ss", type=int)
+    parser.add_argument("-stop", default=-1, required=False, type=int)
+    parser.add_argument("-threads", default=2, required=False, type=int)
+    args = parser.parse_args()
+    incr, ss, stop, th = args.incr, args.ss, args.stop, args.threads
+    ths = tuple(
+        threading.Thread(target=write, args=[range(ss + o * incr, stop, incr * th)])
+        for o in range(0, th)
+    )
+    for t in ths:
+        t.start()
